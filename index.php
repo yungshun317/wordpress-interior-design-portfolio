c1fddd77cfa1201d9cedb665d367978d0a6060dd
yungshun317 <seraph317@gmail.com>
2023-04-02 07:23:35 +0800

    [FEAT] Use `language_attributes`, `bloginfo`, and `body_class` in `index.php`

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset=<?php bloginfo('charset'); ?>>
+    <meta http-equiv="X-UA-Compatible" content="ID=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Interior Design Portfolio</title>
+</head>
+<body <?php body_class(); ?>>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et magna a massa feugiat imperdiet. Donec quam diam, dictum id odio eu, euismod hendrerit ex. Quisque quam elit, scelerisque vitae aliquet consequat, commodo vitae urna. Curabitur aliquam, elit nec congue pretium, velit ipsum interdum nisl, at posuere diam purus ut neque. In lobortis turpis sit amet risus consectetur egestas. Vestibulum aliquet accumsan urna, nec commodo leo consectetur at. Proin quis magna diam. Fusce sollicitudin porttitor mauris, sit amet euismod augue imperdiet eget. Sed tincidunt vitae ex eget auctor. Praesent egestas volutpat risus eu finibus. Nunc a augue lorem.</p>
+    <div id="page" class="site">
+        <!-- Header -->
+        <header>
+            <section class="top-bar">
+                <div class="logo">
+                    Logo
+                </div>
+                <div class="searchbox">
+                    Search
+                </div>
+            </section>
+            <section class="menu-area">
+                <nav class="main-menu">
+                    Menu
+                </nav>
+            </section>
+        </header>
+        <!-- Content -->
+        <div id="content" class="site-content">
+            <div id="primary" class="content-area">
+                <main id="main" class="site-main">
+                    <section class="hero">
+                        Hero
+                    </section>
+                    <section class="services">
+                        Services
+                    </section>
+                    <section class="home-blog">
+                        Blog
+                    </section>
+                </main>
+            </div>
+        </div>
+        <!-- Footer -->
+        <footer class="site-footer">
+            Footer
+        </footer>
+    </div>
+</body>
+</html>
