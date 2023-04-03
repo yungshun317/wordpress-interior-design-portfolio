fbbe0c0d2b8ebc5d384163050c4a9bfa01b3a48b
yungshun317 <seraph317@gmail.com>
2023-04-03 08:19:01 +0800

    [FEAT] Use `register_nav_menu` in `functions.php`

--- footer.php
+++ footer.php
@@ -1,6 +1,7 @@
-<footer class="site-footer">
-    Footer
-</footer>
+    <footer class="site-footer">
+        Footer
+    </footer>
 </div>
+    <?php wp_footer(); ?>
 </body>
 </html>
--- functions.php
+++ functions.php
@@ -5,3 +5,10 @@ function interior_design_portfolio_load_scripts() {
     wp_enqueue_style("google-fonts", "https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap", array(), null);
 }
 add_action("wp_enqueue_scripts", "interior_design_portfolio_load_scripts");
+
+register_nav_menus(
+    array(
+        "interior_design_portfolio_main_menu" => "Main Menu",
+        "interior_design_portfolio_footer_menu" => "Footer Menu"
+    )
+);
