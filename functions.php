eb4d7f4f52bd782b7a420fab7093835799566d8d
yungshun317 <seraph317@gmail.com>
2023-04-03 08:38:27 +0800

    [FEAT] Enqueue scripts in `functions.php` and enable the dropdown menu

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@
 function interior_design_portfolio_load_scripts() {
     wp_enqueue_style("interior-design-portfolio-style", get_stylesheet_uri(), array(), filemtime(get_template_directory() . "/style.css"), "all");
     wp_enqueue_style("google-fonts", "https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap", array(), null);
+    wp_enqueue_script("dropdown", get_template_directory_uri() . "/js/dropdown.js", array(), "1.0", true);
 }
 add_action("wp_enqueue_scripts", "interior_design_portfolio_load_scripts");
 
